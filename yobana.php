2b955aaa1763b249ee78a73f7c3d16a461302304
Kolyn236 <kolyn236@mail.ru>
2016-12-09 19:23:26 +0400

    Add files via upload

---
+++ yobana.php
@@ -0,0 +1,55 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+
+  
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<?header( 'Content-Type: text/html; charset=utf-8' );?>
+<div class="news-list">
+ 
+<?
+CModule::includeModule('iblock');
+//Получаю список секций
+
+$iblockID = 2;
+$arFilterSect = array("IBLOCK_ID" => $iblockID, "ACTIVE"=>"Y","DEPTH_LEVEL" => 1);
+$arSelectSect = array("ID","NAME","DEPTH_LEVEL","IBLOCK_ID");
+$arSelectRow = array("ID","NAME");
+$result = array();
+$arListSec = CIBlockSection::GetList(false,$arFilter,false,$arSelectRow);
+while($arListSect = $arListSec->Fetch())
+{
+  $result[$arListSect['ID']] = $arListSect;
+}      
+$count=1;
+$arSelect = array("NAME","ID","IBLOCK_SECTION_ID","PREVIEW_TEXT");
+$arFilter = array("IBLOCK_ID" => $iblockID,"SECTION_ID" => array_keys($result));
+$arSort = array("IBLOCK_SECTION_ID" => "ASC");
+$arGroupBy = false;
+$res = CIBlockElement::GetList($arSort,$arFilter,$arGroupBy,false, $arSelect);// Получаю нужные поля, сортирую
+while ($ob=$res->GetNext()) {
+      $result[$ob['IBLOCK_SECTION_ID']]['ITEMS'][] = $ob;
+      
+}
+
+
+//}
+//echo "<pre>";
+//print_r($result);
+//echo "</pre>";
+?>
+
+<hr>
+</div>
